3f70a36f481e98d58557e5d0a4d2837aa27cc720
Michael Kölle <michael.koelle@ifi.lmu.de>
2023-05-16 14:19:01 +0200

    Update job scripts

--- jobs/job.sh
+++ jobs/job.sh
@@ -7,12 +7,27 @@
 
 # Use --partition=NvidiaAll if you need nodes with a gpu
 
+# Set Environment Variables
+# export WANDB_MODE="disabled" # Use if you want to disable wandb logging
+export WANDB_SILENT="true"
+
+# Setup pyenv shell
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
 # Create a fresh virtual environment
-pyenv virtualenv env
-pyenv activate env
+pyenv virtualenv env &> /dev/null
+pyenv activate env &> /dev/null
+
+# Check the exit status of the pyenv activate command
+if [ $? -ne 0 ]; then
+    echo "Failed to activate the virtual environment. Exiting."
+    exit 1
+fi
 
 # Makes sure that newly added modules are installed aswell
-pip install -r requirements.txt
+pip install -qr requirements.txt
 
 # Runs the script
 python src/main.py $@
--- jobs/run-jobs.sh
+++ jobs/run-jobs.sh
@@ -1,14 +1,27 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Specify hyperparameters
 runs=20
-envs=("CartPole-v1" "Pendulum-v1")
-agents=("random" "PPO" "DQN" "A2C")
+datasets=("iris" "wine")
+approaches=("random" "OL-NN-EP" "TL-NN-EP" "OL-NN" "TL-NN")
 
-for env in ${envs[@]}; do
-    for agent in ${agents[@]}; do
+# Check if sbatch is available
+if command -v sbatch > /dev/null; then
+  sbatch_cmd="sbatch"
+else
+  echo "\033[1;31mYou are not in a slurm environment. Executing experiments sequentially!\033[0m"
+  sbatch_cmd=""
+fi
+
+for dataset in ${datasets[@]}; do
+    for approach in ${approaches[@]}; do
         for seed in $(seq 0 $(($runs-1))); do
-            sbatch --job-name="run-$env-$agent-$seed" jobs/job.sh -e $env -a $agent -s $seed
+            if [ -z "$sbatch_cmd" ]; then
+                echo "\033[1;32mExecuting job with environment variables:\033[0m -d $dataset -a $approach -s $seed"
+                ./jobs/job.sh -d $dataset -a $approach -s $seed -e $epoch
+            else
+                $sbatch_cmd --job-name="run-$dataset-$approach-$seed" jobs/job.sh -d $dataset -a $approach -s $seed
+            fi
         done
     done
 done
