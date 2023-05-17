ea1324877330088d854746c8333804557ea2586c
Michael Kölle <michael.koelle@ifi.lmu.de>
2023-05-17 09:33:25 +0200

    Add virtualenv to script

--- jobs/job.sh
+++ jobs/job.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/zsh
 
 #SBATCH --mail-user=<INSERT YOUR EMAIL HERE>
 #SBATCH --mail-type=FAIL
@@ -11,18 +11,37 @@
 # export WANDB_MODE="disabled" # Use if you want to disable wandb logging
 export WANDB_SILENT="true"
 
-# Setup pyenv shell
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# Check if pyenv is installed
+if command -v pyenv 1>/dev/null 2>&1; then
+    # Setup pyenv shell
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
 
-# Create a fresh virtual environment
-pyenv virtualenv env &> /dev/null
-pyenv activate env &> /dev/null
+    # Create a fresh virtual environment
+    pyenv virtualenv env 1>/dev/null 2>&1
+    pyenv activate env 1>/dev/null 2>&1
 
-# Check the exit status of the pyenv activate command
-if [ $? -ne 0 ]; then
-    echo "Failed to activate the virtual environment. Exiting."
+    # Check the exit status of the pyenv activate command
+    if [ $? -ne 0 ]; then
+        echo "\033[31mFailed to activate the virtual environment using pyenv. Exiting.\033[0m"
+        exit 1
+    fi
+
+# Check if virtualenv is installed
+elif command -v virtualenv 1>/dev/null 2>&1; then
+    # Create a fresh virtual environment using virtualenv
+    virtualenv env
+    source env/bin/activate
+
+    # Check the exit status of the virtual environment activation
+    if [ $? -ne 0 ]; then
+        echo "\033[31mFailed to activate the virtual environment using virtualenv. Exiting.\033[0m"
+        exit 1
+    fi
+# If neither is installed, quit!
+else
+    echo "\033[31mNeither pyenv nor virtualenv are available. Exiting.\033[0m"
     exit 1
 fi
 
